ea88233afef15e54b327b3a81227ff56909e7a94
danny <kremzan17@gmail.com>
2025-04-12 10:26:09 -0400

    changes to login and assests

--- Front-End/src/app/pages/login/login.component.ts
+++ Front-End/src/app/pages/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  selector: 'app-login', // Used as a custom HTML tag <app-login>
+  templateUrl: './login.component.html', // Links to the HTML file
+  styleUrls: ['./login.component.css'] // Links to the CSS file
 })
 export class LoginComponent {
+  // Component logic goes here
+  username: string = '';
+  password: string = '';
 
+  login() {
+    console.log('Logging in with', this.username, this.password);
+  }
 }
